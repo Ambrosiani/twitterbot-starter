a79d685e4e105500f5c498b929e5544e8e0e834d
Glitch (twitterbot) <none>
2020-04-16 13:57:43 +0000

    🏣⏰ Checkpoint ./server.js:178972/9121

--- server.js
+++ server.js
@@ -1,51 +1,35 @@
 /* Setting things up. */
-let express = require( 'express' ),
-    app = express(),
-    CronJob = require( 'cron' ).CronJob,    
-    Twit = require( 'twit' ),
+var express = require('express'),
+    app = express(),   
+    Twit = require('twit'),
     config = {
     /* Be sure to update the .env file with your API keys. See how to get them: https://botwiki.org/tutorials/how-to-create-a-twitter-app */      
       twitter: {
-        consumer_key: process.env.CONSUMER_KEY,
-        consumer_secret: process.env.CONSUMER_SECRET,
-        access_token: process.env.ACCESS_TOKEN,
-        access_token_secret: process.env.ACCESS_TOKEN_SECRET
+        consumer_key: process.env.TWITTER_CONSUMER_KEY,
+        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+        access_token: process.env.TWITTER_ACCESS_TOKEN,
+        access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
       }
     },
-    T = new Twit( config.twitter );
+    T = new Twit(config.twitter);
 
-app.use( express.static( 'public' ) );
+app.use(express.static('public'));
 
-/*
-  If you have a Glitch subscription ( glitch.com/pricing ) and your app is Always On, you can use the cron package to run your bot automatically.
+/* You can use cron-job.org, uptimerobot.com, or a similar site to hit your /BOT_ENDPOINT to wake up your app and make your Twitter bot tweet. */
 
-  Otherwise you can use cron-job.org, uptimerobot.com, or a similar site to hit your /BOT_ENDPOINT to wake up your app and make your Twitter bot tweet.
-*/
-
-app.all( `/${process.env.BOT_ENDPOINT}`, function( req, res ){
+app.all(`/${process.env.BOT_ENDPOINT}`, function(req, res){
   /* The example below tweets out "Hello world!". */
-  T.post( 'statuses/update', { status: 'hello world 👋' }, function( err, data, response ) {
-    if ( err ){
-      console.log( 'error!', err );
-      res.sendStatus( 500 );
+  T.post('statuses/update', { status: 'hello world 👋' }, function(err, data, response) {
+    if (err){
+      console.log('error!', err);
+      res.sendStatus(500);
     }
     else{
-      res.sendStatus( 200 );
+      res.sendStatus(200);
     }
-  } );
-} );
+  });
+});
 
-let listener = app.listen( process.env.PORT, function(){
-  console.log( 'Your bot is running on port ' + listener.address().port );
-  
-  /*
-    If your app is Always On, you can use the cron package:
-  
-  */
-  let job = new CronJob( '*/5 * * * *', function() {
-    console.log( 'You will see this message every second' );
-  } );
-
-  job.start();
-  
-} );
+var listener = app.listen(process.env.PORT, function(){
+  console.log('Your bot is running on port ' + listener.address().port);
+});
